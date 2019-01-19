42ca6af81d07accaaf545517dd0816f2b4db042f
Francis Karagu <fkaragu@gmail.com>
2019-01-19 22:47:36 +0300

    Added header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GheaderComponent } from './gheader/gheader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GheaderComponent
   ],
   imports: [
     BrowserModule,
