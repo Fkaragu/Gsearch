c019fbe57fdfc3d5d3cb3bf006f0492f4f9d20b9
Francis Karagu <fkaragu@gmail.com>
2019-01-20 07:37:50 +0300

    Added detail linking

--- src/app/gdetail/gdetail.component.ts
+++ src/app/gdetail/gdetail.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Search } from '../search-class/search';
+import { SearchRequestService } from '../search-http/search-request.service'
+
 
 @Component({
   selector: 'app-gdetail',
   templateUrl: './gdetail.component.html',
+  providers: [SearchRequestService],
   styleUrls: ['./gdetail.component.css']
 })
 export class GdetailComponent implements OnInit {
+  quote: Search;
+  constructor(private searchService: SearchRequestService) {
 
-  constructor() { }
+  }
 
   ngOnInit() {
+    this.searchService.searchRequest()
+    this.quote = this.searchService.quote
   }
 
 }
--- src/app/gheader/gheader.component.ts
+++ src/app/gheader/gheader.component.ts
@@ -1,22 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Search } from '../search-class/search';
-import { SearchRequestService } from '../search-http/search-request.service'
 
 @Component({
   selector: 'app-gheader',
   templateUrl: './gheader.component.html',
-  providers:[SearchRequestService],
   styleUrls: ['./gheader.component.css']
 })
 export class GheaderComponent implements OnInit {
-  quote:Search;
 
-  constructor(private searchService:SearchRequestService) {
+  constructor() {
 
   }
   ngOnInit() {
-    this.searchService.searchRequest()
-    this.quote=this.searchService.quote
   }
 }
